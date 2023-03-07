61b203fe9d2326246967bd023b0c3323c6b0ca62
Pedro <pedro.diehl@ayga.com.br>
2023-03-07 15:41:51 -0300

    Corrigido plot dos elos do atuador

--- invKin.m
+++ invKin.m
@@ -34,13 +34,13 @@ function [theta1, theta2] = invKin(eePosition, a1, a2, angleUM, technique)
     px = eePosition(1, 1);
     py = eePosition(2, 1);
  
+    % Confere se o ponto esta na area de trabalho do robo
+    if (sqrt(px^2 + py^2) > (a1 + a2)) || (sqrt(px^2 + py^2) < (a1 - a2))
+        error('The given end effector position is outside of the arm reachable space');
+    end
+  
     switch technique
         case 'algebraic'
-            % Confere se o ponto esta na area de trabalho do robo
-            if (sqrt(px^2 + py^2) > (a1 + a2)) || (sqrt(px^2 + py^2) < (a1 - a2))
-                error('The given end effector position is outside of the arm reachable space');
-            end
-            
             % Calcula os valores trigonométricos para theta2
             c2 = (px^2 + py^2 - a1^2 - a2^2) / (2 * a1 * a2);
             s2 = sqrt(1 - c2^2);
@@ -50,14 +50,11 @@ function [theta1, theta2] = invKin(eePosition, a1, a2, angleUM, technique)
             % Cacula os valores trigonométricos para theta1
             c1 = ((a1 + a2 * c2) * px + a2 * s2 * py) / (px^2 + py^2);
             s1 = ((a1 + a2 * c2) * py - a2 * s2 * px) / (px^2 + py^2);
-            
+
             ang_theta1 = atan2(s1, c1); 
             
         case 'geometric'
-            % Confere se o ponto esta na area de trabalho do robo
-            if sqrt(px^2 + py^2) > (a1 + a2) || (sqrt(px^2 + py^2) < (a1 - a2))
-                error('The given end effector position is outside of the arm reachable space');
-            end
+            disp('geometric TODO');
     
         otherwise
             error('Not valid technique informed');
--- robotica1.m
+++ robotica1.m
@@ -18,40 +18,14 @@ point_a2 = [a2; 0; 0];
 
 x_ee = 2;
 y_ee = 1;
+p_ee = [x_ee; y_ee];
 
-% Defasagem do circulo
-h = 1;
-k = 1;
+[theta1, theta2] = invKin(p_ee, a1, a2, 'deg', 'algebraic')
 
-% Raio do circulo
-r = 0.5;
+posicao_1 = rM('z', theta1, 'deg') * point_a1
+posicao_2 = rM('z', (theta1 + theta2), 'deg') * point_a2
 
-
-for phi = 0:0.1:(2 * pi)
-    % Equacao parametrica para o circulo
-    p_circulo = [(h + r * cos(phi));
-                 (k + r * sin(phi))];
- 
-    
-    %p_circulo = [1; 2];
-    [theta1, theta2] = invKin(p_circulo, a1, a2, 'deg', 'algebraic')
-
-    posicao_1 = rM('z', theta1, 'deg') * point_a1;
-    posicao_2 = rM('z', theta2, 'deg') * point_a2;
-    
-    hold on
-    grid on
-    %quiver(0, 0, posicao_1(1), posicao_1(2), 'r', 'LineWidth', a1);
-    %quiver(posicao_1(1), posicao_1(2), posicao_2(1), posicao_2(2), 'b', 'LineWidth', a2);
-    plot(posicao_1(1), posicao_1(2), 'o');
-    plot(posicao_2(1), posicao_2(2), '*');
-    
-    %angle_between = rad2deg(atan2(norm(cross([posicao_1(1) posicao_1(2) 0],[posicao_2(1), posicao_2(2) 0])), dot([posicao_1(1), posicao_1(2) 0],[posicao_2(1), posicao_2(2) 0])));
-    %text(0, 0, [num2str(theta1), '\circ']);
-    %text(posicao_1(1), posicao_1(2), [num2str(angle_between), '\circ']);
-
-    axis([-3 3 -3 3]);
-    pause(0.5);
-    %plot(x1, y1, '-ro')
-end
+plot([0 posicao_1(1)],  [0 posicao_1(2)], 'r', [posicao_1(1) posicao_2(1)], [posicao_1(2) posicao_2(2)], 'b', 'linewidth', 3);
+grid on
+axis([-3 3 -3 3]);
 
