2952717f61b813e890b5521f3923d84305b226fa
Pedro <pedro.diehl@ayga.com.br>
2023-03-10 13:10:12 -0300

    Adicionado métodos para mover, calcular cinemática inversa e graficar os bracos

--- RobotRR.m
+++ RobotRR.m
@@ -22,7 +22,117 @@ classdef RobotRR
    
    % MÉTODOS MÉTODOS MÉTODOS MÉTODOS
    methods
+       % CINEMÁTICA INVERSA CINEMÁTICA INVERSA CINEMÁTICA INVERSA CINEMÁTICA INVERSA
+       function [theta1, theta2] = inverseKinematics(obj, eePosition, angleUM, technique)
+           % Decompoe o vetor em x e y 
+           px = eePosition(1, 1);
+           py = eePosition(2, 1);
 
+           % Confere se o ponto esta na area de trabalho do robo
+           if (sqrt(px^2 + py^2) > (obj.links(1).size + obj.links(2).size)) ||...
+               (sqrt(px^2 + py^2) < (obj.links(1).size - obj.links(2).size))
+               error('The given end effector position is outside of the arm reachable space');
+           end
+
+           switch technique
+               case 'algebraic'
+                   % Calcula os valores trigonométricos para theta2
+                   c2 = (px^2 + py^2 - obj.links(1).size^2 - obj.links(2).size^2) / (2 * obj.links(1).size * obj.links(2).size);
+
+                   % Confere se o ponto esta na area de trabalho do robo
+                   if (c2 > 1) || (c2 < -1)
+                       error('The given end effector position is outside of the arm reachable space');
+                   end
+
+                   s2 = sqrt(1 - c2^2);
+
+                   ang_theta2 = atan2(s2, c2);
+
+                   % Cacula os valores trigonométricos para theta1
+                   c1 = ((obj.links(1).size + obj.links(2).size * c2) * px + obj.links(2).size * s2 * py) / (px^2 + py^2);
+                   s1 = ((obj.links(1).size + obj.links(2).size * c2) * py - obj.links(2).size * s2 * px) / (px^2 + py^2);
+
+                   ang_theta1 = atan2(s1, c1); 
+
+               case 'geometric'
+                   disp('TODO');
+
+               otherwise
+                   error('Not valid technique informed');
+           end
+
+           switch angleUM
+               case 'rad'
+                   theta1 = ang_theta1;
+                   %theta1 = round(ang_theta1, 2);
+
+                   theta2 = ang_theta2;
+                   %theta2 = round(ang_theta2, 2);
+
+               case 'deg'
+                   theta1 = rad2deg(ang_theta1);
+                   %theta1 = round(rad2deg(ang_theta1), 2);
+
+                   theta2 = rad2deg(ang_theta2);
+                   %theta2 = round(rad2deg(ang_theta2), 2);
+
+               otherwise
+                   error('Not valid angle unit informed.');
+           end
+       end
+       % CINEMÁTICA INVERSA CINEMÁTICA INVERSA CINEMÁTICA INVERSA CINEMÁTICA INVERSA
+       
+       
+       % MOVER MANIPULADOR MOVER MANIPULADOR MOVER MANIPULADOR MOVER MANIPULADOR
+       function moveManipulator(obj, pDesired)
+           startPos = obj.links(1).endPos + obj.links(2).endPos;
+
+           % Vetor de distancia do deslocamento
+           dist = pDesired - startPos;
+
+           % Magnitude da hipotenusa para avancar sobre
+           hipMag = sqrt(dist(1)^2 + dist(2)^2 + dist(3)^2);
+
+           for p = 0:0.1:hipMag
+               % Decompoe a hipotenusa em x e y atraves do uso de cossenos e senos
+               % Posicao atual + ou -, cosseno ou seno da posicao atual na hipotenusa
+               pDesloc = startPos + p * (dist / hipMag);
+
+               [theta1, theta2] = invKin(pDesloc,...
+                                         obj.links(1).size,...
+                                         obj.links(2).size,...
+                                         'deg',...
+                                         'algebraic');
+
+               obj.links(1).updateEndPos(rM('z', theta1, 'deg'), theta1);
+               obj.links(2).updateEndPos(rM('z', (theta1 + theta2), 'deg'), theta2);
+
+               grid on
+               figure(1)
+               obj.plotArm(obj.links(1).endPos, obj.links(2).endPos);
+               axis([-3 3 -3 3]);
+               
+               figure(3)
+               plot((obj.links(1).endPos(1) + obj.links(2).endPos(1)),...
+                    (obj.links(1).endPos(2) + obj.links(2).endPos(2)),...
+                    'o', 'linewidth', 0.5);
+               hold on
+               axis([-3 3 -3 3]);
+
+               %pause(1)
+               pause(5e-12);
+           end
+       end
+       % MOVER MANIPULADOR MOVER MANIPULADOR MOVER MANIPULADOR MOVER MANIPULADOR
+       
+       function plotArm(obj)
+           figure(1)
+           plot([0 obj.links(1).endPos(1)], [0 obj.links(1).endPos(2)], 'r',...
+               [obj.links(1).endPos(1) (obj.links(1).endPos(1) + obj.links(2).endPos(1))], [obj.links(1).endPos(2) (obj.links(1).endPos(2) + obj.links(2).endPos(2))], 'b',...
+               'linewidth', 5);
+           grid on
+           axis([-3 3 -3 3]);
+       end
    end
    % MÉTODOS MÉTODOS MÉTODOS MÉTODOS
 end
---
+++ example.m
@@ -0,0 +1,28 @@
+%{
+Universidade Federal de Pelotas
+
+Professor: Alexandre Molter
+Aluno: Pedro Henrique Diehl
+
+Exemplo de uso
+%}
+
+format short
+clc, close all, clear all
+
+% Parametros dos elos do robo
+a1 = 3; % Comprimento do primeiro elo
+a2 = 3; % Comprimento do segundo elo
+
+x_ee = 1;
+y_ee = 1;
+endEffectorPos = [x_ee;
+                  y_ee;
+                  0];
+
+% Cria uma instancia do robo
+robot = RobotRR(a1, a2);
+
+% Move o manipulador para a posicao desejada
+robot.moveManipulator(endEffectorPos);
+
