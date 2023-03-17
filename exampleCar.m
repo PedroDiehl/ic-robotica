2542a383fcde9aeadeca3ec91799186af8eae7c7
Pedro <pedro.diehl@ayga.com.br>
2023-03-17 15:01:51 -0300

    Adicionado exemplo de desenho de carrinho

--- exampleCar.m
+++ exampleCar.m
@@ -17,8 +17,22 @@ a2 = 3; % Comprimento do segundo elo
 % Cria uma instancia do robo
 robot = RobotRR(a1, a2);
 
-robot.moveManipulator([3; 0; 0]);
+robot.moveManipulator([5; 0; 0]);
+robot.drawLine([5; -1; 0]);
+robot.drawLine([4; -2; 0]);
+robot.drawLine([3; -2; 0]);
+robot.drawHorizontalHalfCircle(2, -2, 1, 1, 1); 
+robot.drawLine([-1; -2; 0]);
+robot.drawHorizontalHalfCircle(-2, -2, 1, 1, 1); 
+robot.drawLine([-4; -2; 0]);
+robot.drawLine([-5; -1; 0]);
+robot.drawLine([-5; 0; 0]);
+robot.drawLine([-4; 0; 0]);
+robot.drawLine([-3; 2; 0]);
+robot.drawLine([3; 2; 0]);
+robot.drawLine([4; 0; 0]);
+robot.drawLine([5; 0; 0]);
+robot.drawCircle(2, -2, 0.75, 1); 
+robot.drawCircle(-2, -2, 0.75, 1); 
 
-
-robot.drawLine([3; 0; 0]);
 robot.moveManipulatorToRest();
